5cd1e00e85c7ab6a7eba84087e6feee7deb1ee20
Jonathan Leibovich <60627566+leamur88@users.noreply.github.com>
2022-04-10 16:15:31 -0500

    pyling at 6.19

--- calc.py
+++ calc.py
@@ -1,52 +1,51 @@
 class Calculator:
-	def __init__(self):
-		return
+    def __init__(self):
+        return
 
-	def calc(self):
-		while 1:
-			print("What basic operation would you like to do")
-			n1 = self.collectNum("first")
-			operator = self.collectOperator()
-			n2 = self.collectNum("second")
-			if (operator == "+"):
-				self.add(n1,n2)
-			elif (operator == "-"):
-				self.subtract(n1,n2)
-			elif (operator == "/"):
-				self.divide(n1,n2)
-			elif (operator == "*"):
-				self.multiply(n1,n2)
+    def calc(self):
+        while 1:
+            print("What basic operation would you like to do")
+            number1 = self.collect_num("first")
+            operator = self.collect_operator()
+            number2 = self.collect_num("second")
+            if operator == "+":
+                self.add(number1, number2)
+            elif operator == "-":
+                self.subtract(number1, number2)
+            elif operator == "/":
+                self.divide(number1, number2)
+            elif operator == "*":
+                self.multiply(number1, number2)
 
+    def collect_num(self, string):
+        try:
+            return int(input(f"Enter your {string} number:\n"))
+        except ValueError:
+            raise ValueError
 
-	def collectNum(self, s):
-		try:
-			return int(input(f"Enter your {s} number:\n"))
-		except ValueError:
-			raise ValueError
+    def collect_operator(self):
+        i = input("Enter desired operator number:\n")
+        if i not in ["+", "-", "/", "*"]:
+            raise ValueError
+        return i
 
-	def collectOperator(self):
-		i = input(f"Enter desired operator number:\n")
-		if i not in ["+", "-", "/", "*"]:
-			raise ValueError
-		return i
+    def add(self, number1, number2):
+        print(f"{number1} + {number2} = {number1 + number2}")
+        return number1 + number2
 
-	def add(self, n1, n2):
-		print(f"{n1} + {n2} = {n1 + n2}")
-		return n1 + n2
+    def subtract(self, number1, number2):
+        print(f"{number1} - {number2} = {number1 - number2}")
+        return number1 - number2
 
-	def subtract(self, n1, n2):
-		print(f"{n1} - {n2} = {n1 - n2}")
-		return n1 - n2
+    def divide(self, number1, number2):
+        print(f"{number1} / {number2} = {number1 / number2}")
+        return number1 / number2
 
-	def divide(self, n1, n2):
-		print(f"{n1} / {n2} = {n1 / n2}")
-		return n1 / n2
-
-	def multiply(self, n1, n2):
-		print(f"{n1} * {n2} = {n1 * n2}")
-		return n1 * n2
+    def multiply(self, number1, number2):
+        print(f"{number1} * {number2} = {number1 * number2}")
+        return number1 * number2
 
 
 if __name__ == '__main__':
-	c = Calcluator()
-	c.calc()
+    c = Calculator()
+    c.calc()
